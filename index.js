8e27b4cd6df549d284318907c3de0fc63961b5fc
haakoaho <haakoaho@stud.ntnu.no>
2017-09-06 11:25:35 +0200

    Satte opp en server

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var express=require("express");
-var port=8081
+var port=8081;
+var app=express();
+var http = require('http').Server(app);
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+http.listen(port,function(){
+  console.log('Listening on '+port);
+});
