316e8648f89475e21d682ce40dceed9985498b5e
Ine Lofthus <inelofthus@gmail.com>
2017-09-07 13:37:08 +0200

    Testet ut ajax og lagt til public folder til bilder, stylesheets og script

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ var app=express();
 var http = require('http').Server(app);
 
 app.get('/', function(req,res){
-  res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname+'/blabla.html');
 });
 http.listen(port,function(){
   console.log('Listening on '+port);
 });
+
+app.get('/ajaxcall', function(req, res){
+	var data = {
+		contactId: 1,
+		firstname: 'John',
+		lastName: 'Doe',
+		email: 'john.doe@domain.com',
+		phone: '987654'
+	};
+	res.send(data);
+});
+
+
+//Setup for using static files (stylesheets, images, scripts etc.) from public directory
+app.use(express.static('public'));
